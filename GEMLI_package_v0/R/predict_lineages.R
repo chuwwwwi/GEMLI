06bfbeba837b406db2e79c2453f55d2e9d30ca3a
chuwwwwi <69376936+chuwwwwi@users.noreply.github.com>
2024-05-04 13:45:34 -0400

    Update predict_lineages.R

--- GEMLI_package_v0/R/predict_lineages.R
+++ GEMLI_package_v0/R/predict_lineages.R
@@ -2,12 +2,22 @@ predict_lineages <- function(GEMLI_items, repetitions=100, sample_size=(2/3), de
 {
   data_matrix = GEMLI_items[['gene_expression']]
   marker_genes = potential_markers(data_matrix)
-  results = data.matrix(matrix(0, nrow=ncol(data_matrix), ncol=ncol(data_matrix))); rownames(results) = colnames(data_matrix); colnames(results) = colnames(data_matrix)
+  
+  results = data.matrix(matrix(0, nrow=ncol(data_matrix), ncol=ncol(data_matrix)))
+  rownames(results) = colnames(data_matrix)
+  colnames(results) = colnames(data_matrix)
+  
   for (i in seq(1,repetitions))
   {
-    marker_genes_sample = sample(intersect(marker_genes, rownames(data_matrix)), round(length(intersect(marker_genes, rownames(data_matrix)))*sample_size,0))
+    marker_genes_sample = sample(intersect(marker_genes, rownames(data_matrix)), 
+                                 round(length(intersect(marker_genes, rownames(data_matrix)))*sample_size,0))
     cell_clusters = quantify_clusters_iterative(data_matrix, marker_genes_sample, N=2, fast)
-    cell_clusters_unique_name = cell_clusters; for (colname in 1:ncol(cell_clusters)){cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname] = paste0(colname,'_',cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname])}
+    cell_clusters_unique_name = cell_clusters
+    
+    for (colname in 1:ncol(cell_clusters)) {
+      cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname] = paste0(colname,'_',cell_clusters_unique_name[!is.na(cell_clusters_unique_name[,colname]),colname])
+    }
+    
     clustersize_dict = table(cell_clusters_unique_name)
     smallest_clusters = names(clustersize_dict)[clustersize_dict %in% desired_cluster_size]
     best_prediction = data.matrix(matrix(F, nrow=ncol(data_matrix), ncol=ncol(data_matrix))); rownames(best_prediction) = colnames(data_matrix); colnames(best_prediction) = colnames(data_matrix)
